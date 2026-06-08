5a06dc91ee3e86a11eec8ed671ce9be7f139d07c
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-07 21:05:31 -0400

    Wire scenario and clock panels

--- game/scripts/MainDashboard.cs
+++ game/scripts/MainDashboard.cs
@@ -2,9 +2,8 @@ using Godot;
 namespace RestaurantSimulator;
 public partial class MainDashboard:Control{
  public override void _Ready(){
-  var box=new VBoxContainer();AddChild(box);
-  box.SetAnchorsPreset(LayoutPreset.FullRect);
+  var box=new VBoxContainer();AddChild(box);box.SetAnchorsPreset(LayoutPreset.FullRect);
   box.AddChild(new Label{Text="Restaurant Simulator"});
-  box.AddChild(new Label{Text="Phase 1 shell: scenario, clock, POS, KDS, labor, inventory, SOS, alerts pending."});
- }
-}
+  var clock=new ClockPanel();
+  var start=new Button{Text="Start/Pause"};start.Pressed+=clock.Toggle;
+  box.Add
