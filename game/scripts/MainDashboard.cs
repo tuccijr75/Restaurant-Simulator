2c98f93696f2336f4346866750a47e359905fcd6
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-08 06:31:48 -0400

    Wire dashboard to run state

--- game/scripts/MainDashboard.cs
+++ game/scripts/MainDashboard.cs
@@ -2,10 +2,11 @@ using Godot;
 namespace RestaurantSimulator;
 public partial class MainDashboard:Control{
  public override void _Ready(){
-  var b=new VBoxContainer();AddChild(b);
+  var st=new SimRunState();var b=new VBoxContainer();AddChild(b);
   b.AddChild(new Label{Text="Restaurant Simulator"});
-  var c=new ClockPanel();var s=new Button{Text="Start/Pause"};s.Pressed+=c.Toggle;
-  b.AddChild(new ScenarioPanel());b.AddChild(s);b.AddChild(c);
+  var sc=new ScenarioPanel();sc.Bind(st);var c=new ClockPanel();c.Bind(st);
+  var btn=new Button{Text="Start/Pause"};btn.Pressed+=()=>st.Running=!st.Running;
+  b.AddChild(sc);b.AddChild(btn);b.AddChild(c);
   b.AddChild(new PosPanel());b.AddChild(new KdsPanel());b.AddChild(new LaborPanel());
   b.AddChild(new InventoryPanel());b.AddChild(new SosPanel());b.AddChild(new AlertPanel());
  }
