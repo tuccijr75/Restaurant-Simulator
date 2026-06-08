ccb298e5fc4bed7ca76c2aa6da13afb6d1d75de7
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-08 06:13:47 -0400

    Wire dashboard placeholder panels

--- game/scripts/MainDashboard.cs
+++ game/scripts/MainDashboard.cs
@@ -4,8 +4,9 @@ public partial class MainDashboard:Control{
  public override void _Ready(){
   var b=new VBoxContainer();AddChild(b);
   b.AddChild(new Label{Text="Restaurant Simulator"});
-  var c=new ClockPanel();var s=new Button{Text="Start/Pause"};
-  s.Pressed+=c.Toggle;
+  var c=new ClockPanel();var s=new Button{Text="Start/Pause"};s.Pressed+=c.Toggle;
   b.AddChild(new ScenarioPanel());b.AddChild(s);b.AddChild(c);
+  b.AddChild(new PosPanel());b.AddChild(new KdsPanel());b.AddChild(new LaborPanel());
+  b.AddChild(new InventoryPanel());b.AddChild(new SosPanel());b.AddChild(new AlertPanel());
  }
 }
