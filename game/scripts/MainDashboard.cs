514d5e9d683e7c1cff84c3293d81752ed5771285
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-08 11:15:34 -0400

    Wire JSONL file export button

--- game/scripts/MainDashboard.cs
+++ game/scripts/MainDashboard.cs
@@ -6,10 +6,10 @@ public partial class MainDashboard:Control{
   var sc=new ScenarioPanel();sc.Bind(st);var c=new ClockPanel();c.Bind(st);
   var p=new PosPanel();p.Bind(st);var k=new KdsPanel();k.Bind(st);
   var sp=new StationPanel();sp.Bind(st);var so=new SosPanel();so.Bind(st);
-  var a=new AlertPanel();a.Bind(st);var e=new EventPanel();e.Bind(st);var j=new JsonlPanel();j.Bind(st);
+  var a=new AlertPanel();a.Bind(st);var e=new EventPanel();e.Bind(st);var j=new JsonlPanel();j.Bind(st);var x=new ExportPanel();x.Bind(st);
   var btn=new Button{Text="Start/Pause"};btn.Pressed+=()=>st.Running=!st.Running;
   b.AddChild(new Label{Text="Restaurant Simulator"});b.AddChild(sc);b.AddChild(btn);b.AddChild(c);
   b.AddChild(p);b.AddChild(k);b.AddChild(sp);b.AddChild(new LaborPanel());
-  b.AddChild(new InventoryPanel());b.AddChild(so);b.AddChild(a);b.AddChild(e);b.AddChild(j);
+  b.AddChild(new InventoryPanel());b.AddChild(so);b.AddChild(a);b.AddChild(e);b.AddChild(j);b.AddChild(x);
  }
 }
