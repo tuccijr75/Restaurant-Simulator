71b22afc1246f3ee9dcb616796780e1b8231bb7e
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-08 07:07:19 -0400

    Bind live panels

--- game/scripts/MainDashboard.cs
+++ game/scripts/MainDashboard.cs
@@ -2,12 +2,19 @@ using Godot;
 namespace RestaurantSimulator;
 public partial class MainDashboard:Control{
  public override void _Ready(){
-  var st=new SimRunState();var b=new VBoxContainer();AddChild(b);
+  var st=new SimRunState();
+  var b=new VBoxContainer();
+  AddChild(b);
+  var sc=new ScenarioPanel();sc.Bind(st);
+  var c=new ClockPanel();c.Bind(st);
+  var p=new PosPanel();p.Bind(st);
+  var k=new KdsPanel();k.Bind(st);
+  var so=new SosPanel();so.Bind(st);
+  var btn=new Button{Text="Start/Pause"};
+  btn.Pressed+=()=>st.Running=!st.Running;
   b.AddChild(new Label{Text="Restaurant Simulator"});
-  var sc=new ScenarioPanel();sc.Bind(st);var c=new ClockPanel();c.Bind(st);
-  var btn=new Button{Text="Start/Pause"};btn.Pressed+=()=>st.Running=!st.Running;
   b.AddChild(sc);b.AddChild(btn);b.AddChild(c);
-  b.AddChild(new PosPanel());b.AddChild(new KdsPanel());b.AddChild(new LaborPanel());
-  b.AddChild(new InventoryPanel());b.AddChild(new SosPanel());b.AddChild(new AlertPanel());
+  b.AddChild(p);b.AddChild(k);b.AddChild(new LaborPanel());
+  b.AddChild(new InventoryPanel());b.AddChild(so);b.AddChild(new AlertPanel());
  }
 }
