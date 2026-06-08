bedb6737030ccad5a8381e1861b6bb14a89224d3
tuccijr75 <michaelrobertucci@gmail.com>
2026-06-07 20:23:28 -0400

    Add main dashboard script

---
+++ game/scripts/MainDashboard.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace RestaurantSimulator;
+
+public partial class MainDashboard : Control
+{
+    public override void _Ready()
+    {
+        GD.Print("Restaurant Simulator shell ready.");
+    }
+}
